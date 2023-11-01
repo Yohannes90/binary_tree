996397d18e794930a61a09851418f573277c94b7
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-11-01 23:05:10 +0300

    second commit for question 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,10 +12,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-	size_t n_left, n_right;
 
-	n_left = binary_tree_size(tree->left);
-	n_right = binary_tree_size(tree->right);
-
-	return (1 + n_left + n_right);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
